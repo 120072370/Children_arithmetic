262f8bf1ee2f1b19228da2ef00f42e8704e5e49e
Shikeemac <120072370@qq.com>
2018-09-10 10:58:25 +0800

    no message

--- client/utils/PlayMusic.js
+++ client/utils/PlayMusic.js
@@ -6,19 +6,19 @@ const TouChAudio = wx.createInnerAudioContext(),//点击
 
   function play(index){
     if (index == 0) {
-       TouChAudio.src = "/mp3/点击音效01.mp3";
+       TouChAudio.src = "/mp3/过关.m4a";
        TouChAudio.play();
     }else if(index == 1){
-       PressAudio.src = "/mp3/点击音效02.mp3";
+       PressAudio.src = "/mp3/过关.m4a";
        PressAudio.play();
     }else if(index == 2){
-       PassAudio.src = "/mp3/过关音效.mp3";
+       PassAudio.src = "/mp3/过关.m4a";
        PassAudio.play();
     }else if(index == 3){
-       PromptAudio.src = "/mp3/题目错误.mp3";
+       PromptAudio.src = "/mp3/过关.m4a";
        PromptAudio.play();
     }else{
-       FailureAudio.src = "/mp3/失败音效01.mp3";
+       FailureAudio.src = "/mp3/过关.m4a";
        FailureAudio.play();
     }
   }
